077d6cd138f1e0006012f892492202b884be3861
xdpeix <xdpeix@gmail.com>
2021-08-16 13:33:48 +0300

    Implemented role manager

---
+++ Chatto.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,16 @@
+﻿using Chatto.DAL.Entities;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatto.DAL.Identity
+{
+	class ApplicationRoleManager : RoleManager<ApplicationRole>
+	{
+		public ApplicationRoleManager(IRoleStore<ApplicationRole> store) : base(store)
+		{ }
+	}
+}
