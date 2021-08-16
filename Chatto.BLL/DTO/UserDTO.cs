e53958a64bab902d448e9a9b9cf960f7efd13ff7
xdpeix <xdpeix@gmail.com>
2021-08-16 14:53:00 +0300

    Created DTO (data transfer object) class for user data

---
+++ Chatto.BLL/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Chatto.BLL.DTO
+{
+	class UserDTO
+	{
+		public string Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public string Gender { get; set; }
+		public int Age { get; set; }
+	}
+}
