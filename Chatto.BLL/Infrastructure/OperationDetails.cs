299ee3f67c65b320ddae2a3ce2437b40079d117d
xdpeix <xdpeix@gmail.com>
2021-08-16 15:18:52 +0300

    Implemented OperationDetails for error logging

---
+++ Chatto.BLL/Infrastructure/OperationDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatto.BLL.Infrastructure
+{
+	class OperationDetails
+	{
+		public bool Succeeded { get; private set; }
+		public string Message { get; private set; }
+		public string Property { get; private set; }
+
+		public OperationDetails (bool succeeded, string message, string property)
+		{
+			Succeeded = succeeded;
+			Message = message;
+			Property = property;
+		}
+	}
+}
