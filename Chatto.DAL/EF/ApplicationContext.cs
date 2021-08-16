86a79a060d6a22eb8baeff968d5be8bf36d8e75c
xdpeix <xdpeix@gmail.com>
2021-08-16 12:48:04 +0300

    Added EF context

---
+++ Chatto.DAL/EF/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Chatto.DAL.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatto.DAL.EF
+{
+	class ApplicationContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationContext(string connectionString) : base(connectionString)
+		{ }
+
+		public DbSet<ClientProfile> ClientProfiles { get; set; }
+	}
+}
