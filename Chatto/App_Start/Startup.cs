b8865b3df44e162e2b66b5b97e8f9ccf98c537b1
xdpeix <xdpeix@gmail.com>
2021-08-17 12:33:36 +0300

    Added OWIN Startup class

---
+++ Chatto/App_Start/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Chatto.BLL.Interfaces;
+using Chatto.BLL.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Chatto.App_Start.Startup))]
+
+namespace Chatto.App_Start
+{
+	public class Startup
+	{
+		readonly IServiceCreator serviceCreator = new ServiceCreator();
+		public void Configuration(IAppBuilder app)
+		{
+			app.CreatePerOwinContext<IUserService>(CreateUserService);
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString("Account/Login")
+			});
+		}
+
+		private IUserService CreateUserService()
+		{
+			return serviceCreator.CreateUserService("IdentityDb");
+		}
+	}
+}
