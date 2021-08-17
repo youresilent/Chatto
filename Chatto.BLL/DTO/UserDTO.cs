c39d8baf96fa61ed592d6dac0b8a718e6b759c3b
xdpeix <xdpeix@gmail.com>
2021-08-17 11:34:35 +0300

    Implemented services

--- Chatto.BLL/DTO/UserDTO.cs
+++ Chatto.BLL/DTO/UserDTO.cs
@@ -5,9 +5,11 @@
 		public string Id { get; set; }
 		public string UserName { get; set; }
 		public string Password { get; set; }
+		public string RealName { get; set; }
 		public string Email { get; set; }
 		public string Address { get; set; }
 		public string Gender { get; set; }
 		public int Age { get; set; }
+		public string Role { get; set; }
 	}
 }
