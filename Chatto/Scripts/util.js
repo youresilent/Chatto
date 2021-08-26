63bcd7d90e0a984654b42e9fc969c06ac37912dd
xdpeix <xdpeix@gmail.com>
2021-08-26 12:58:03 +0300

    Fixed client-side JS functions

--- Chatto/Scripts/util.js
+++ Chatto/Scripts/util.js
@@ -2,43 +2,52 @@
 
 	var hubVar = $.connection.signalHub;
 
-	$.connection.hub.start().done(function () {
-		console.log('connected!' + hubVar.connection.id)
-	})
-
 	hubVar.client.showNotification = function (userName, message) {
 		document.getElementById('notificationBlock').removeAttribute('hidden');
 		document.getElementById('notificationBlock').append(htmlEncode(userName) + ' ' + htmlEncode(message));
-		$.fn.playNotificationSound();
+		$.fn.playSound();
 
 		setTimeout(function () {
 			window.location.reload();
 		}, 2000);
 	};
 
-	$.fn.playNotificationSound = function () {
-		const audio = new Audio('/Sounds/ping.wav');
+	$.fn.playSound = function () {
+		const audio = new Audio("/Sounds/ping.wav");
 		audio.play();
-	};
+	}
 
-	$.fn.playMessageSound = function () {
-		const audio = new Audio('/Sounds/message.wav');
-		audio.play();
-	};
+	hubVar.client.onNewUserConnected = function (id, name) {
+		AddUser(id, name);
+	}
 
-	hubVar.client.onConnected = function (id, userName, users) {
-		for (i = 0; i < users.length; i++) {
-			AddUser(users[i].ConnectionId, users[i].UserName);
+	hubVar.client.onConnected = function (id, userName, allUsers) {
+
+		for (i = 0; i < allUsers.length; i++) {
+
+			AddUser(allUsers[i].ConnectionId, allUsers[i].User);
 		}
-	};
-
-	hubVar.client.onNewUserConnected = function (id, userName) {
-		AddUser(id, userName);
-	};
+	}
 
 	hubVar.client.onUserDisconnected = function (id, userName) {
+
 		$('#' + id).remove();
-	};
+	}
+
+	$.connection.hub.start().done(function () {
+
+		console.log('connected! ' + hubVar.connection.id);
+
+		hubVar.server.connect(document.getElementById('userName').getAttribute('value'));
+
+		$('#addFriendNotification').click(function () {
+			hubVar.server.sendNotification(document.getElementById('friendUserName').getAttribute('value'), 'Someone added you to their friendslist! Reloading page in 2 seconds');
+		});
+
+		$('#removeFriendNotification').click(function () {
+			hubVar.server.sendNotification(document.getElementById('friendUserName').getAttribute('value'), 'Someone removed you from their friendslist! Reloading page in 2 seconds');
+		});
+	})
 
 	function htmlEncode(value) {
 		var encodedValue = $('<div />').text(value).html();
