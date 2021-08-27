82c2ad17dc15df77454c85fe28c2c5e52c7a7480
xdpeix <xdpeix@gmail.com>
2021-08-27 10:21:45 +0300

    AccountController refactoring

--- Chatto/Controllers/AccountController.cs
+++ Chatto/Controllers/AccountController.cs
@@ -5,9 +5,7 @@ using Chatto.Hubs;
 using Chatto.Models;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,23 +13,23 @@ using System.Web.Mvc;
 
 namespace Chatto.Controllers
 {
-    public class AccountController : Controller
-    {
-        #region init
+	public class AccountController : Controller
+	{
+		#region init
 
-        private IUserService UserService
+		private IUserService UserService
 		{
 			get
 			{
-                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+				return HttpContext.GetOwinContext().GetUserManager<IUserService>();
 			}
 		}
 
-        private IAuthenticationManager AuthenticationManager
+		private IAuthenticationManager AuthenticationManager
 		{
-            get
+			get
 			{
-                return HttpContext.GetOwinContext().Authentication;
+				return HttpContext.GetOwinContext().Authentication;
 			}
 		}
 
@@ -39,39 +37,39 @@ namespace Chatto.Controllers
 
 		#region registration
 
-        public ViewResult Register()
+		public ViewResult Register()
 		{
-            return View();
+			return View();
 		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register (RegisterModel registerModel)
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Register(RegisterModel registerModel)
 		{
-            //await SetInitialDataAsync();
-            if (ModelState.IsValid)
+			//await SetInitialDataAsync();
+			if (ModelState.IsValid)
 			{
-                UserDTO userDTO = new UserDTO
-                {
-                    UserName = registerModel.UserName,
-                    Password = registerModel.Password,
-                    RealName = registerModel.RealName,
-                    Email = registerModel.Email,
-                    Adress = registerModel.Adress,
-                    Gender = registerModel.Gender,
-                    Age = registerModel.Age,
-                    Role = "user"
-                };
+				UserDTO userDTO = new UserDTO
+				{
+					UserName = registerModel.UserName,
+					Password = registerModel.Password,
+					RealName = registerModel.RealName,
+					Email = registerModel.Email,
+					Adress = registerModel.Adress,
+					Gender = registerModel.Gender,
+					Age = registerModel.Age,
+					Role = "user"
+				};
 
-                OperationDetails operation = await UserService.Create(userDTO);
+				OperationDetails operation = await UserService.Create(userDTO);
 
-                if (operation.Succeeded)
-                    return RedirectToAction("LogIn");
-                else
-                    ModelState.AddModelError(operation.Property, operation.Message);
+				if (operation.Succeeded)
+					return RedirectToAction("LogIn");
+				else
+					ModelState.AddModelError(operation.Property, operation.Message);
 			}
 
-            return View(registerModel);
+			return View(registerModel);
 		}
 
 		#endregion
@@ -79,60 +77,59 @@ namespace Chatto.Controllers
 		#region login/logout
 		public RedirectToRouteResult LogOut()
 		{
-            AuthenticationManager.SignOut();
-            return RedirectToAction("Index", "Home");
+			AuthenticationManager.SignOut();
+
+			return RedirectToAction("Index", "Home");
 		}
 
-        public ViewResult LogIn()
+		public ViewResult LogIn()
 		{
-            return View();
+			return View();
 		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> LogIn(LogInModel loginModel)
-        {
-            if (ModelState.IsValid)
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> LogIn(LogInModel loginModel)
+		{
+			if (ModelState.IsValid)
 			{
-                UserDTO userDTO = new UserDTO
-                {
-                    UserName = loginModel.UserName,
-                    Password = loginModel.Password
-                };
-
-                ClaimsIdentity claims = await UserService.Authenticate(userDTO);
-
-                if (claims == null)
-                    ModelState.AddModelError("", "Неверная комбинация логин/пароль");
-                else
+				UserDTO userDTO = new UserDTO
 				{
-                    AuthenticationManager.SignOut();
-                    AuthenticationManager.SignIn(new AuthenticationProperties
-                    {
-                        IsPersistent = true
-                    }, claims);
+					UserName = loginModel.UserName,
+					Password = loginModel.Password
+				};
 
-                    return RedirectToAction("Home");
-                }
+				ClaimsIdentity claims = await UserService.Authenticate(userDTO);
+
+				if (claims == null)
+					ModelState.AddModelError("", "Wrong login/password input!");
+				else
+				{
+					AuthenticationManager.SignOut();
+					AuthenticationManager.SignIn(new AuthenticationProperties
+					{
+						IsPersistent = true
+					}, claims);
+
+					return RedirectToAction("Home");
+				}
 
 			}
 
-            return View(loginModel);
-        }
+			return View(loginModel);
+		}
 
 		#endregion
 
 		#region homepage
 
-        [Authorize]
-        public ViewResult Home()
+		[Authorize]
+		public ViewResult Home()
 		{
-            var zxc = User.Identity.Name;
-            UserDTO user = GetUserData(User.Identity.Name);
-            var friends = GetUserFriends();
-            ViewBag.Friends = friends;
+			UserDTO user = GetUserData(User.Identity.Name);
+			ViewBag.Friends = GetUserFriends();
 
-            return View(user);
+			return View(user);
 		}
 
 		#endregion
@@ -140,127 +137,124 @@ namespace Chatto.Controllers
 		#region profile-methods
 
 		[Authorize]
-        [Route("Account/ProfileInfo/{userName}")]
-        public ViewResult ProfileInfo(string userName)
+		[Route("Account/ProfileInfo/{userName}")]
+		public ViewResult ProfileInfo(string userName)
 		{
-            UserDTO user = GetUserData(userName);
-            return View(user);
+			return View(GetUserData(userName));
 		}
 
-        [Authorize]
-        public ViewResult ProfileEdit()
+		[Authorize]
+		public ViewResult ProfileEdit()
 		{
-            UserDTO user = GetUserData(User.Identity.Name);
-            return View(user);
+			return View(GetUserData(User.Identity.Name));
 		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ProfileEdit(UserDTO newUser)
-        {
-            if (string.IsNullOrWhiteSpace(newUser.Adress))
-                ModelState.AddModelError("Adress", "Adress is required!");
-
-            if (string.IsNullOrWhiteSpace(newUser.Email))
-                ModelState.AddModelError("Email", "E-mail adress is required!");
-
-            if (string.IsNullOrWhiteSpace(newUser.RealName))
-                ModelState.AddModelError("RealName", "Real name is required!");
-
-            if (string.IsNullOrWhiteSpace(newUser.Gender))
-                ModelState.AddModelError("Gender", "Gender is required!");
-
-            if (newUser.Age < 5 || newUser.Age > 120)
-                ModelState.AddModelError("Age", "Age input is not correct! It must be between 5 and 120.");
-
-            if (ModelState.IsValid)
-			{
-                UserService.ChangeSecondaryInfo(newUser);
-                return RedirectToAction("Home");
-            }
-            else
-			{
-                return View(newUser);
-            }
-        }
-
-        [Authorize]
-        public ViewResult ChangePassword()
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult ProfileEdit(UserDTO newUser)
 		{
-            return View();
+			if (string.IsNullOrWhiteSpace(newUser.Adress))
+				ModelState.AddModelError("Adress", "Adress is required!");
+
+			if (string.IsNullOrWhiteSpace(newUser.Email))
+				ModelState.AddModelError("Email", "E-mail adress is required!");
+
+			if (string.IsNullOrWhiteSpace(newUser.RealName))
+				ModelState.AddModelError("RealName", "Real name is required!");
+
+			if (string.IsNullOrWhiteSpace(newUser.Gender))
+				ModelState.AddModelError("Gender", "Gender is required!");
+
+			if (newUser.Age < 5 || newUser.Age > 120)
+				ModelState.AddModelError("Age", "Age input is not correct! It must be between 5 and 120.");
+
+			if (ModelState.IsValid)
+			{
+				UserService.ChangeSecondaryInfo(newUser);
+				return RedirectToAction("Home");
+			}
+			else
+				return View(newUser);
 		}
 
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ChangePassword(ChangePasswordModel model)
+		[Authorize]
+		public ViewResult ChangePassword()
 		{
-            if (ModelState.IsValid)
-			{
-                UserDTO user = GetUserData(User.Identity.Name);
-                OperationDetails operation = UserService.ChangePassword(model.OldPassword, model.NewPassword, user.Id);
+			return View();
+		}
 
-                if (operation.Succeeded)
-                    return RedirectToAction("Home");
-                else
-                    ModelState.AddModelError(operation.Property, operation.Message);
+		[Authorize]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult ChangePassword(ChangePasswordModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				UserDTO user = GetUserData(User.Identity.Name);
+				OperationDetails operation = UserService.ChangePassword(model.OldPassword, model.NewPassword, user.Id);
+
+				if (operation.Succeeded)
+					return RedirectToAction("Home");
+				else
+					ModelState.AddModelError(operation.Property, operation.Message);
 			}
 
-            return View(model);
+			return View(model);
 		}
 
-        [Authorize]
-        public ViewResult PeopleList()
+		[Authorize]
+		public ViewResult PeopleList()
 		{
-            ViewBag.Users = UserService.GetAllUsers();
-            return View(GetUserData(User.Identity.Name));
+			ViewBag.Users = UserService.GetAllUsers();
+
+			return View(GetUserData(User.Identity.Name));
 		}
 
-        [Authorize]
-        public ViewResult FriendsList()
+		[Authorize]
+		public ViewResult FriendsList()
 		{
-            ViewBag.Friends = GetUserFriends();
+			ViewBag.Friends = GetUserFriends();
 
-            return View();
+			return View();
 		}
 
-        [Authorize]
-        public ActionResult AddFriend(string friendUserName)
+		[Authorize]
+		public ActionResult AddFriend(string friendUserName)
 		{
-            string currentUserName = User.Identity.Name;
+			string currentUserName = User.Identity.Name;
 
-            OperationDetails operation = UserService.AddFriend(currentUserName, friendUserName);
+			OperationDetails operation = UserService.AddFriend(currentUserName, friendUserName);
 
-            if (operation.Succeeded)
+			if (operation.Succeeded)
 			{
-                SignalHub.Static_SendNotification(currentUserName, friendUserName, "has added you to their friendslist! Refreshing page...");
-                return RedirectToAction("Home");
+				SignalHub.Static_SendNotification(currentUserName, friendUserName, "has added you to their friendslist! Refreshing page...");
+				return RedirectToAction("Home");
 			}
-            else
-                return Redirect("/Shared/Error.cshtml");
+			else
+				return Redirect("/Shared/Error.cshtml");
 		}
 
-        [Authorize]
-        public ActionResult RemoveFriend(string friendUserName)
+		[Authorize]
+		public ActionResult RemoveFriend(string friendUserName)
 		{
-            string currentUserName = User.Identity.Name;
+			string currentUserName = User.Identity.Name;
 
-            OperationDetails operation = UserService.RemoveFriend(User.Identity.Name, friendUserName);
+			OperationDetails operation = UserService.RemoveFriend(User.Identity.Name, friendUserName);
 
-            if (operation.Succeeded)
+			if (operation.Succeeded)
 			{
-                SignalHub.Static_SendNotification(currentUserName, friendUserName, "has removed you from their friendslist! Refreshing page...");
-                return RedirectToAction("Home");
+				SignalHub.Static_SendNotification(currentUserName, friendUserName, "has removed you from their friendslist! Refreshing page...");
+				return RedirectToAction("Home");
 			}
-            else
-                return Redirect("/Shared/Error.cshtml");
+			else
+				return Redirect("/Shared/Error.cshtml");
 
-        }
+		}
 
-        [Authorize]
-        public ViewResult DeleteAccount()
+		[Authorize]
+		public ViewResult DeleteAccount()
 		{
-            return View();
+			return View();
 		}
 
 		[HttpPost]
@@ -268,68 +262,68 @@ namespace Chatto.Controllers
 		public ActionResult DeleteAccount(string confirmation)
 		{
 			if (User.Identity.Name != confirmation)
-                return View("Error");
+				return Redirect("/Shared/Error.cshtml");
 
-            var operation = UserService.DeleteAccount(confirmation);
+			var operation = UserService.DeleteAccount(confirmation);
 
-            LogOut();
+			LogOut();
 
-            if (!operation.Succeeded)
-                return View("Error");
+			if (!operation.Succeeded)
+				return Redirect("/Shared/Error.cshtml");
 
-            return RedirectToAction("Index", "Home");
-        }
-
-        #endregion
-
-        //private async Task SetInitialDataAsync()
-        //{
-        //	await UserService.SetInitialData(new BLL.DTO.UserDTO
-        //	{
-        //		Adress = "ADMINADRESS",
-        //		Age = 10,
-        //		UserName = "adminadmin",
-        //		Email = "ADMINEMAIL",
-        //		RealName = "ADMINREALNAME",
-        //		Gender = "ADMINGENDER",
-        //		Password = "123123",
-        //		Role = "admin"
-        //	}, new List<string> { "user", "admin" });
-        //}
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        private UserDTO GetUserData(string userName)
-		{
-            return UserService.GetUserData(userName);
+			return RedirectToAction("Index", "Home");
 		}
 
-        private List<UserDTO> GetUserFriends()
+		#endregion
+
+		#region non-action methods
+
+		//private async Task SetInitialDataAsync()
+		//{
+		//	await UserService.SetInitialData(new BLL.DTO.UserDTO
+		//	{
+		//		Adress = "ADMINADRESS",
+		//		Age = 10,
+		//		UserName = "adminadmin",
+		//		Email = "ADMINEMAIL",
+		//		RealName = "ADMINREALNAME",
+		//		Gender = "ADMINGENDER",
+		//		Password = "123123",
+		//		Role = "admin"
+		//	}, new List<string> { "user", "admin" });
+		//}
+
+		private UserDTO GetUserData(string userName)
 		{
-            var user = GetUserData(User.Identity.Name);
-            var friends = StringToList(user.Friends);
-            List<UserDTO> friendsDTOs = new List<UserDTO>();
+			return UserService.GetUserData(userName);
+		}
 
-            foreach (var friend in friends)
-                friendsDTOs.Add(GetUserData(friend.UserName));
-
-            return friendsDTOs;
-        }
-
-        private List<UserDTO> StringToList(string friendsList)
+		private List<UserDTO> GetUserFriends()
 		{
-            List<string> stringList = UserService.StringToList(friendsList);
-            List<UserDTO> users = new List<UserDTO>();
+			var user = GetUserData(User.Identity.Name);
+			var friends = StringToList(user.Friends);
 
-            foreach(var user in stringList)
+			List<UserDTO> friendsDTOs = new List<UserDTO>();
+
+			foreach (var friend in friends)
+				friendsDTOs.Add(GetUserData(friend.UserName));
+
+			return friendsDTOs;
+		}
+
+		private List<UserDTO> StringToList(string friendsList)
+		{
+			List<string> stringList = UserService.StringToList(friendsList);
+			List<UserDTO> users = new List<UserDTO>();
+
+			foreach (var user in stringList)
 			{
-                users.Add(GetUserData(user));
+				users.Add(GetUserData(user));
 			}
 
-            return users;
+			return users;
 		}
-    }
+
+		#endregion
+	}
 }
