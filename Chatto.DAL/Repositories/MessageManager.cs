8614c29f13e2f874bbb313202fa29751aa2a7ee1
xdpeix <xdpeix@gmail.com>
2021-08-30 13:24:33 +0300

    Implemented MessageManager interface

---
+++ Chatto.DAL/Repositories/MessageManager.cs
@@ -0,0 +1,38 @@
+﻿using Chatto.DAL.EF;
+using Chatto.DAL.Entities;
+using Chatto.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatto.DAL.Repositories
+{
+	public class MessageManager : IMessageManager
+	{
+		public ApplicationContext DataBase { get; set; }
+
+		public MessageManager(ApplicationContext db)
+		{
+			DataBase = db;
+		}
+
+		public void Create(ClientMessage message)
+		{
+			DataBase.ClientMessages.Add(message);
+			DataBase.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			DataBase.Dispose();
+		}
+
+		public void Remove(ClientMessage message)
+		{
+			DataBase.ClientMessages.Remove(message);
+			DataBase.SaveChanges();
+		}
+	}
+}
