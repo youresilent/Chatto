72c6ab345a7e0d1a41df7b32301bcdb2266ef00f
xdpeix <xdpeix@gmail.com>
2021-08-30 13:51:33 +0300

    Created MessageDTO

---
+++ Chatto.BLL/DTO/MessageDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatto.BLL.DTO
+{
+	public class MessageDTO
+	{
+		public string Id { get; set; }
+
+		public string Sender { get; set; }
+
+		public string Recipient { get; set; }
+
+		public string Message { get; set; }
+
+		public DateTime SendDateTime { get; set; }
+	}
+}
