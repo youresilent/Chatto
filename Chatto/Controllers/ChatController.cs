160f9838a0ed5e88365fdf931527f6cff2ec0bc5
xdpeix <xdpeix@gmail.com>
2021-08-31 14:48:22 +0300

    Installed AutoFac package, created config for AutoFac, created controller for "Chat" functionality

---
+++ Chatto/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using Chatto.BLL.DTO;
+using Chatto.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chatto.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly IMessageService _messageService;
+
+        public ChatController(IMessageService messageService)
+		{
+            _messageService = messageService;
+		}
+
+        // GET: Chat
+        public ActionResult ChatRoom()
+        {
+            _messageService.Create(new MessageDTO { Id = "0", Message = "zxc", Recipient = "recipient", Sender = "sender", SendDateTime = DateTime.Now });
+            return Content(_messageService.ToString());
+        }
+    }
+}
