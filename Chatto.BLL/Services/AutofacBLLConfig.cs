f8e7a71e55b30075eebecd7618c859f3a60f2568
xdpeix <xdpeix@gmail.com>
2021-08-31 14:47:33 +0300

    Installed AutoFac package to BL-layer and created config file for AutoFac

---
+++ Chatto.BLL/Services/AutofacBLLConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Chatto.BLL.Interfaces;
+using Chatto.BLL.Services;
+using Chatto.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chatto.BLL.Services
+{
+	public class AutofacBLLConfig
+	{
+		public static ContainerBuilder ConfigureBuilderInBLL()
+		{
+			var builder = new ContainerBuilder();
+
+			builder.RegisterType<MessageService>().As<IMessageService>().WithParameter("uow", new IdentityUnitOfWork());
+
+			return builder;
+		}
+	}
+}
