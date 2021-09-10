03981285f04ed43861e9004316c791621d2b0bb3
xdpeix <xdpeix@gmail.com>
2021-09-10 15:16:32 +0300

    Fixed SendMessage method

--- Chatto/Controllers/ChatController.cs
+++ Chatto/Controllers/ChatController.cs
@@ -10,8 +10,8 @@ using System.Web.Mvc;
 
 namespace Chatto.Controllers
 {
-    public class ChatController : Controller
-    {
+	public class ChatController : Controller
+	{
 		private readonly IMessageService _messageService;
 
 		public ChatController(IMessageService messageService)
@@ -20,24 +20,35 @@ namespace Chatto.Controllers
 		}
 
 		public ActionResult ChatRoom(string friendUserName)
-        {
+		{
 			List<MessageDTO> messages = _messageService.GetMessages(User.Identity.Name, friendUserName);
 			ViewBag.FriendUserName = friendUserName;
 
 			return View(messages);
-        }
+		}
 
-        [HttpPost]
-        public ActionResult ChatAction(string messageText, string currentUserName, string friendUserName)
+		[HttpPost]
+		public ActionResult SendMessage(string messageText, string friendUserName)
 		{
+			var currentUserName = User.Identity.Name;
 			var message = GetMessageDTO(currentUserName, friendUserName, messageText, DateTime.Now);
 
 			var operation = _messageService.Create(message);
 
-			SignalHub.Static_SendMessage(message, currentUserName);
-			SignalHub.Static_SendMessage(message, friendUserName);
+			if (operation.Result.Succeeded)
+			{
+				SignalHub.Static_SendMessage(message, currentUserName);
+				SignalHub.Static_SendMessage(message, friendUserName);
 
-			return Content(operation.Result.Message);
+				SignalHub.Static_SendMessageNotification(friendUserName, currentUserName);
+
+				return Content("Message sent successfully!");
+			}
+			else
+			{
+				return Content("Message was not sent!");
+			}
+
 		}
 
 		private MessageDTO GetMessageDTO(string sender, string recipient, string message, DateTime sendDateTime)
