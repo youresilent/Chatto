a00bcb321eaf7bbd424c0723587edff1106fff0e
xdpeix <xdpeix@gmail.com>
2021-09-10 17:01:49 +0300

    P-layer code cleanup (#2)

--- Chatto/Controllers/ChatController.cs
+++ Chatto/Controllers/ChatController.cs
@@ -1,11 +1,8 @@
 ﻿using Chatto.BLL.DTO;
 using Chatto.BLL.Interfaces;
 using Chatto.Hubs;
-using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Chatto.Controllers
