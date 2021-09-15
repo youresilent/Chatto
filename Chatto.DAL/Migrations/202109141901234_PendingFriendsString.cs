82a65fb087e0538b5dffd792c54b95c03243394f
xdpeix <xdpeix@gmail.com>
2021-09-15 18:04:38 +0300

    DA-layer - Added fields for 'friend' notifications (PendingFriendsSent & PendingFriendsReceived)

---
+++ Chatto.DAL/Migrations/202109141901234_PendingFriendsString.cs
@@ -0,0 +1,18 @@
+﻿namespace Chatto.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PendingFriendsString : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ClientProfiles", "PendingFriends", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ClientProfiles", "PendingFriends");
+        }
+    }
+}
---
+++ Chatto.DAL/Migrations/202109151007310_PendingFriendsReceiveSendStrings.cs
@@ -0,0 +1,22 @@
+﻿namespace Chatto.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PendingFriendsReceiveSendStrings : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ClientProfiles", "PendingFriendsSent", c => c.String());
+            AddColumn("dbo.ClientProfiles", "PendingFriendsReceived", c => c.String());
+            DropColumn("dbo.ClientProfiles", "PendingFriends");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ClientProfiles", "PendingFriends", c => c.String());
+            DropColumn("dbo.ClientProfiles", "PendingFriendsReceived");
+            DropColumn("dbo.ClientProfiles", "PendingFriendsSent");
+        }
+    }
+}
