ee92602fa6b4a27605accfd65936ab12f93bbaf1
xdpeix <xdpeix@gmail.com>
2021-09-22 01:43:45 +0300

    DA-layer - Added ClientPendingFriend DbSet and set up modelbinder (1 ClientProfile - many ClientPendingFriend)

--- Chatto.DAL/EF/ApplicationContext.cs
+++ Chatto.DAL/EF/ApplicationContext.cs
@@ -11,18 +11,26 @@ namespace Chatto.DAL.EF
 		public DbSet<ClientProfile> ClientProfiles { get; set; }
 		public DbSet<ClientMessage> ClientMessages { get; set; }
 
+		public DbSet<ClientPendingFriend> ClientPendingFriends { get; set; }
+
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<ApplicationUser>()
-				.HasRequired(c => c.ClientProfile)
-				.WithRequiredPrincipal(c => c.ApplicationUser);
+				.HasRequired(r => r.ClientProfile)
+				.WithRequiredPrincipal(p => p.ApplicationUser);
 
 			modelBuilder.Entity<ClientMessage>()
 				.HasKey(k => k.Id);
 
 			modelBuilder.Entity<ClientMessage>()
-				.Property(k => k.Id)
+				.Property(p => p.Id)
 				.HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+			modelBuilder.Entity<ClientProfile>()
+				.HasMany(m => m.ClientPendingFriends)
+				.WithRequired(w => w.ClientProfile)
+				.HasForeignKey(k => k.Id_Receiver)
+				.WillCascadeOnDelete(false);
 
 			base.OnModelCreating(modelBuilder);
 		}
