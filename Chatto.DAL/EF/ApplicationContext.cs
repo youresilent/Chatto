f7a46ce3d1e79b16fabbb15cd833d403cccfb40b
xdpeix <xdpeix@gmail.com>
2021-09-22 23:02:36 +0300

    DA-layer - fixed many-to-many relationship between ClientProfile and ClientPendingFriend

--- Chatto.DAL/EF/ApplicationContext.cs
+++ Chatto.DAL/EF/ApplicationContext.cs
@@ -26,11 +26,33 @@ namespace Chatto.DAL.EF
 				.Property(p => p.Id)
 				.HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
 
-			modelBuilder.Entity<ClientProfile>()
-				.HasMany(m => m.ClientPendingFriends)
-				.WithRequired(w => w.ClientProfile)
-				.HasForeignKey(k => k.Id_Receiver)
+			modelBuilder.Entity<ClientPendingFriend>()
+				.HasKey(pf => new { pf.Id_Receiver, pf.Id_Sender });
+
+			modelBuilder.Entity<ClientPendingFriend>()
+				.HasRequired(r => r.SenderClientProfile)
+				.WithMany(m => m.ClientSenderPendingFriends)
+				.HasForeignKey(fk => fk.Id_Sender)
 				.WillCascadeOnDelete(false);
+
+			modelBuilder.Entity<ClientPendingFriend>()
+				.HasRequired(r => r.ReceiverClientProfile)
+				.WithMany(m => m.ClientReceiverPendingFriends)
+				.HasForeignKey(fk => fk.Id_Receiver)
+				.WillCascadeOnDelete(false);
+
+			modelBuilder.Entity<ClientPendingFriend>()
+				.Property(p => p.Id)
+				.HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+
+			//modelBuilder.Entity<ClientProfile>()
+			//	.HasMany(m => m.ClientPendingFriends)
+			//	.WithRequired(w => w.ClientProfile)
+			//	.HasForeignKey(k => k.Id_Receiver)
+			//	.WillCascadeOnDelete(false);
+
+			//modelBuilder.Entity<ClientPendingFriend>()
+			//	.HasMany(m => m.ClientProfile)
 
 			base.OnModelCreating(modelBuilder);
 		}
