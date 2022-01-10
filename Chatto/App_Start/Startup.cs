e917c82c64599dc173a0f65f1cedfb58162475cc
youresilent <youresilent@outlook.com>
2022-01-10 16:28:40 +0300

    P layer - updated startup class - authentification now knows that identity ID is GUID

--- Chatto/App_Start/Startup.cs
+++ Chatto/App_Start/Startup.cs
@@ -1,9 +1,13 @@
 ﻿using Chatto.BLL.Interfaces;
 using Chatto.BLL.Services;
+using Chatto.DAL.Entities;
+using Chatto.DAL.Identity;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using System;
 
 [assembly: OwinStartup(typeof(Chatto.App_Start.Startup))]
 
@@ -18,7 +22,16 @@ namespace Chatto.App_Start
 			app.UseCookieAuthentication(new CookieAuthenticationOptions
 			{
 				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-				LoginPath = new PathString("/Account/Login")
+				LoginPath = new PathString("/Account/Login"),
+				Provider = new CookieAuthenticationProvider
+				{
+					OnValidateIdentity = SecurityStampValidator
+						.OnValidateIdentity<ApplicationUserManager, ApplicationUser, Guid>(
+					validateInterval: TimeSpan.FromMinutes(30),
+					regenerateIdentityCallback: (manager, user) =>
+						user.GenerateUserIdentityAsync(manager),
+					getUserIdCallback: (id) => Guid.Parse(id.GetUserId()))
+				}
 			});
 
 			app.MapSignalR();
